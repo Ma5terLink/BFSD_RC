c9b4da37fd16bd4b15db6561d948cab8693f3d05
Ma5terLink <devalon@yandex.ru>
2021-09-04 13:22:18 +0500

    =01= see README.md

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -18,26 +18,47 @@ $("a[href^='#up']").click(function () {
     return false;
 });
 
-/* window.addEventListener('DOMContentLoaded', () => {
-const menuSalonsBtn = document.querySelector('.menu__linkbar-btn'),
-        salonsLinks = document.querySelector('.ourSalonsLinks'),
-        salonsItem = document.querySelectorAll('.ourSalonsLinks__item'),
-        salonsOverlay = document.querySelector('.ourSalonsLinks__overlay');
-    
-    salonsItem.forEach(item => {
-        item.addEventListener('click', () => {
-            menuSalonsBtn.classList.toggle('active');
-            salonsLinks.classList.toggle('active');
-        });
-    });
-    salonsOverlay.addEventListener('click', () => {
-        salonsLinks.classList.toggle('active');
-    });
+// Настройка переключателя сайтов (ОБЩИЙ)
+const   siteB = document.querySelector('.siteBF'),
+        siteR = document.querySelector('.siteRC'),
+        siteToggle = document.querySelectorAll('.site-toggler__item');
 
-    menuSalonsBtn.addEventListener('click', () => {
-        salonsLinks.classList.toggle('active');
+siteToggle.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        item.previousElementSibling.classList.toggle('hide');
+        item.classList.toggle('active');
     });
-}); */
+});
+siteToggle.forEach(item => {
+    item.addEventListener('mouseleave', () => {
+        item.classList.toggle('active');
+        item.previousElementSibling.classList.toggle('hide');
+    });
+});
+siteToggle.forEach(item => {
+    item.addEventListener('click', () => {
+    siteB.classList.toggle('active');
+    siteR.classList.toggle('active');
+    });
+});
+// Настройка работы главного меню сайта BF
+const menuBF = document.querySelectorAll('.menu__item');
+
+menuBF.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        menuBF[4].lastElementChild.style = 'left: 32px';    
+        // item[4].lastElementChild.style = "left: 87px";
+        item.lastElementChild.classList.toggle('active');
+    });
+});
+menuBF.forEach(item => {
+    item.addEventListener('mouseleave', () => {
+        item.lastElementChild.classList.toggle('active');
+    });
+});
+
+
+
 
 
 });
